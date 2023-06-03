a823f37d04fd4f8b57157ce54070ff6b6ea0962e
Mas'ud <masud@youverify.co>
2023-06-03 15:39:03 +0100

    Initial Commit

---
+++ RideApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RideApp
+//
+//  Created by Masud Onikeku on 03/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
